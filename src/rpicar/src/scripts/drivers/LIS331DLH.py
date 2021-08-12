3612cc62088929162beb193fbc9ad3c7f4d1d678
Owluska <kagirina@gmail.com>
2021-08-12 14:51:23 +0100

    Data rate, hp_filter added

--- src/rpicar/src/scripts/drivers/LIS331DLH.py
+++ src/rpicar/src/scripts/drivers/LIS331DLH.py
@@ -22,25 +22,33 @@ class LIS331DLH():
 
     STATUS_REG = 0x27
     #Bytes
-    FS0 = 0x10 
-    FS1 = 0x20 
-
+    #ctrl1
     X_EN = 0x01 
     Y_EN = 0x02 
-    Z_EN = 0x04 
-    
+    Z_EN = 0x04
+    DR0 = 0x08
+    DR1 = 0x10
     PM0 = 0x20 
     PM1 = 0x40 
-    PM2 = 0x80 
-    
-      
+    PM2 = 0x80
+    #ctrl2
+    FDS = 0x10
+    HPM0 = 0x20
+    HPM1 = 0x40
+    HPCF1 = 0x02
+    HPCF0 = 0x01 
+    #ctrl4 
+    FS0 = 0x10 
+    FS1 = 0x20
+    BDU = 0x80 
+    #status      
     XDA = 0x01
     YDA = 0x02
     ZDA = 0x04
     ZYXDA = 0x08
     ZYXOR = 0x80 
 
-    BDU = 0x08
+
     
     GRAVITY_EARTH = 9.80665
 
@@ -58,16 +66,40 @@ class LIS331DLH():
                 self.value = value
                 self.name = name
                 self.scale = self.value *  2/ (2 ** 16)
+
+        
+        class filter_freq:
+            def __init__(self, name, coef, bit_value, data_rate):
+                self.name = name
+                self.coef = coef
+                self.bits = bit_value
+                self.freq = data_rate / self.coef  
+
+        class data_rate:
+            def __init__(self, name, value, bits):
+                self.name = name
+                self.value = value
+                self.bits = bits
         
         self.range_2G = range(    0x00,     0x00, 2, '2G')
         self.range_4G = range(    0x00, self.FS0, 4, '4G')
         self.range_8G = range(self.FS1, self.FS0, 8, '8G')
+
+        self.rate_50 = data_rate('50_Hz', 50, 0x00)
+        self.rate_100 = data_rate('100_Hz', 100, self.DR0)
+        self.rate_400 = data_rate('400_Hz', 400, self.DR1)
+        self.rate_1000 = data_rate('1000_Hz', 1000, self.DR0 | self.DR1)
+
+
+        self.hp_modes = {'reference':self.HPM0, 'normal':self.HPM1}
+        # self.data_rate = {'50Hz': 0x00, '100Hz': self.DR0, '400Hz': self.DR1, '1000Hz': self.DR0, }
+        # self.hp_freqs = {'50Hz':0x00, 'k100':self.HPCF0, 'k200':self.HPCF1, 'k400':self.HPCF1|self.HPCF0}
         
         self.bus = smbus.SMBus(1) # start comm with i2c bus
-        self.range = self.range_2G
         self.scale_factor = self.range_2G.scale 
         
-        self.setup()
+        self.setup(self.rate_1000)
+        self.hp_freq = filter_freq('to_400', 400, self.HPCF1|self.HPCF0, self.rate_1000.value)
 
         self.x = 0.0
         self.y = 0.0
@@ -78,28 +110,29 @@ class LIS331DLH():
         self.rz = 0.0
 
     
-    def setup(self):
+    def setup(self, data_rate, block_reading = True):
         #enable x, y, z axes
         data = self.X_EN | self.Y_EN | self.Z_EN
         # power mode - normal
         data |= self.PM0
+        #set data rate
+        data |= data_rate.bits
+        #print(data)
         self.bus.write_byte_data(self.SLAVE_ADDRESS, self.CTRL_REG1, data)
-        self.bus.write_byte_data(self.SLAVE_ADDRESS, self.CTRL_REG4, self.BDU)
+        if block_reading:
+            self.bus.write_byte_data(self.SLAVE_ADDRESS, self.CTRL_REG4, self.BDU)
+    
+    def hp_filter_setup(self, freq, mode):
+        data = self.bus.read_byte_data(self.SLAVE_ADDRESS, self.CTRL_REG2)
+        data |= mode | self. FDS | freq 
+        self.bus.write_byte_data(self.SLAVE_ADDRESS, self.CTRL_REG2, data)
     
     def set_range(self, range):         
         data = self.bus.read_byte_data(self.SLAVE_ADDRESS, self.CTRL_REG4)
         data |= range.FS0 | range.FS1 
         self.bus.write_byte_data(self.SLAVE_ADDRESS, self.CTRL_REG4, data)
         self.scale_factor = range.scale
-        
-
-    # def acc_sleep(toSleep):
-    #     data = self.bus.read_byte_data(self.SLAVE_ADDRESS, self.CTRL_REG1)
-    #     if toSleep:
-    #         data &= ~self.PM0 & 0xFF
-    #     else:
-    #         data |= self.PM0
-    #     self.bus.write_byte(self.CTRL_REG1, data)
+              
 
     def readXYZ(self):
         status = self.bus.read_byte_data(self.SLAVE_ADDRESS, self.STATUS_REG)
@@ -174,8 +207,8 @@ class LIS331DLH():
 
 
 acc = LIS331DLH()
-#acc.set_range(acc.range_8G)
+acc.hp_filter_setup(acc.hp_freq.bits, acc.hp_modes['reference'])
 acc.readXYZ()
 acc.raw_to_ms()
-print(acc.scale_factor)
+
 print(acc.x, acc.y, acc.z)
